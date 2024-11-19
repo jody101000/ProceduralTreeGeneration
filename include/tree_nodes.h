0aeb66edfd7eaca41cf40f6007021683a89ffefb
Di Zhou <dizhou.zd@gmail.com>
2024-11-18 20:02:05 -0800

    Create tree node structure

---
+++ include/tree_nodes.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <vector>
+#include "attraction_points.h"
+
+struct TreeNode {
+	glm::vec3 position;
+	// std::vector<AttractionPoint> linked_points;
+};
+
+class TreeNodeManager {
+
+};
